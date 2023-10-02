3ad00e5800808eaa7e1f52f26ad65b477c7e3b3a
Geibinger <friedl.jak@gmail.com>
2023-10-02 21:23:50 +0200

    more or less working solution

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 
 #include "config.h"
 
+// Define pin for built-in LED
+#define LED_BUILTIN 2
+
 // Define pins for audio board (MAX98357A)
 #define MAX98357A_DOUT 25
 #define MAX98357A_BCLK 27
@@ -30,15 +33,63 @@ VL6180x sensor(VL6180X_ADDRESS);
 // Audio board
 Audio audio;
 
-// Method to print the measured distance of the VL6180X sensor
-void printDistance()
+// Method to get random filename from SD card in directory
+String getRandomFilename(char* directory)
 {
-    Serial.print("Distance (mm): ");
-    Serial.println(sensor.getDistance());
+    File root = SD.open(directory);
+    File file = root.openNextFile();
+    int count = 0;
+    while (file)
+    {
+        count++;
+        file = root.openNextFile();
+    }
+    root.close();
+    root = SD.open(directory);
+    int random = rand() % count;
+    count = 0;
+    file = root.openNextFile();
+    while (file)
+    {
+        if (count == random)
+        {
+            root.close();
+            return file.name();
+        }
+        count++;
+        file = root.openNextFile();
+    }
+    root.close();
+    return "";
+}
+
+// Method to play random audio file from SD card in directory
+void playRandomAudio(char* directory)
+{
+    if (audio.isRunning())
+        return;
+    String filename = getRandomFilename(directory);
+    if (filename != "")
+    {
+        Serial.print("Playing ");
+        Serial.println(filename);
+        // Check if file ends with .wav
+        if (filename.endsWith(".mp3"))
+        {
+            // append directory to filename (e.g. /smallmp3/1.mp3)
+            filename = String(directory) + "/" + filename;
+            audio.connecttoFS(SD, filename.c_str());
+        }
+        else
+        {
+            Serial.println("File is not .mp3");
+        }
+    }
 }
 
 void setup()
 {
+    // Initialize SD card
     pinMode(SD_CS, OUTPUT);
     digitalWrite(SD_CS, HIGH);
     SPI.begin(SD_SCK, SD_MISO, SD_MOSI);
@@ -47,11 +98,41 @@ void setup()
     {
         Serial.println("Error talking to SD card!");
         while (true)
-            ; // end program
+            ;
     }
+
+    // Initialize audio board
     audio.setPinout(MAX98357A_BCLK, MAX98357A_LRC, MAX98357A_DOUT);
-    audio.setVolume(15); // 0...21
-    audio.connecttoFS(SD, "/nggyu.mp3");
+    audio.setVolume(21);
+
+    // Initialize VL6180X sensor
+    Wire.begin();
+    delay(100);
+
+    if (sensor.VL6180xInit() != 0)
+    {
+        Serial.println("Failed to initialize. Freezing...");
+        while (1)
+            ;
+    }
+
+    sensor.VL6180xDefautSettings();
 }
 
-void loop() { audio.loop(); }
+void loop()
+{
+    if (sensor.getDistance() < 20.) // big money
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        playRandomAudio("/bigmp3");
+    }
+    else if (sensor.getDistance() < 55.) // small money
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        playRandomAudio("/smallmp3");
+    }
+    else
+        digitalWrite(LED_BUILTIN, LOW);
+
+    audio.loop();
+}
